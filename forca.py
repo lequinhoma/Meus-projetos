d2a3b81dd556e860d3d38541eb743bdaa3231b73
lequinhoma <89463891+lequinhoma@users.noreply.github.com>
2023-04-24 22:25:23 -0230

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,7 @@
+def jogar_forca():
+    print ("*"*80)
+    print ("Bem Vindo ao jogo de forca")
+    print ("*"*80)
+    print("Fim de Jogo!")
+    
+    return "panela"
---
+++ main.py
@@ -0,0 +1,34 @@
+import jogodeadivinhacao2
+import forca
+import pedra_papel_tesoura
+
+print ("*"*80)
+print ("Escolha seu jogo!!!")
+print ("*"*80)
+
+print("""Escolha entre: 
+      (A) Jogo da forca
+      (B) Jogo de adivinhação
+      (C)\"Pedra, papel e tesoura\"""")
+
+jogo= input ("Qual jogo? (A, B ou C)\n")
+
+if jogo == "A" or jogo == "a":
+    print ("Jogando FORCA")
+    forca.jogar_forca()
+    exit()
+    
+elif jogo == "B" or jogo == "b":
+    print ("jogando Adivinhação")
+    jogodeadivinhacao2.jogar()
+    exit()
+    
+elif jogo == "C" or jogo== "c":
+
+    print ("Jogando \"Pedra, papel ou tesoura\"")
+    pedra_papel_tesoura.game()
+    
+    exit()
+    
+    
+else: "Você não inseriu uma entrada válida"
